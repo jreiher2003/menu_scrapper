d806ac7761bed8d88f1f9181f5dad4685fcac253
Jeff Reiher <jreiher2003@yahoo.com>
2018-02-23 02:40:56 +0000

    Delete test app

--- test_app/app/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-db = SQLAlchemy(app)
-from app import views
-from app.models import *
